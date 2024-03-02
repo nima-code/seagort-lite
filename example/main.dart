b365c0a5ab589e265eff810598fe38ba8e5a726f
Nike <nikechukwu@gmail.com>
2024-03-02 10:52:29 -0500

    Example Utilization

--- example/main.dart
+++ example/main.dart
@@ -3,17 +3,13 @@ import 'dart:io';
 
 import 'package:path/path.dart' as path;
 import 'package:seagort/seagort.dart';
+import 'package:seagort/src/js.dart';
 
 void main() {
-  var libraryPath = path.join(Directory.current.path, 'tools', 'awesome.so');
-  // if (Platform.isMacOS) {
-  //   libraryPath = path.join(
-  //       Directory.current.path, 'primitives_library', 'libprimitives.dylib');
-  // }
-  // if (Platform.isWindows) {
-  //   libraryPath = path.join(
-  //       Directory.current.path, 'primitives_library', 'Debug', 'primtives.dll');
-  // }
-  DynamicLibrary.open(libraryPath);
-  print(add(2, 1));
+  print(compileJS('''
+
+let b = 2;
+
+let a = 1;
+'''));
 }
