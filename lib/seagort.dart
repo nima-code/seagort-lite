dcbb95a1a0e1f209a707ad17ba81bd1f060e0fd4
Nike <nikechukwu@gmail.com>
2024-03-02 10:52:21 -0500

    Function Implementation in Go

--- lib/seagort.dart
+++ lib/seagort.dart
@@ -1,4 +1,1 @@
 library seagort;
-
-export 'src/libraries/native_add_library.dart';
-export 'src/libraries/native_go_library.dart';
---
+++ lib/src/lib/compile.dart
@@ -0,0 +1,19 @@
+import 'dart:ffi';
+import 'package:ffi/ffi.dart';
+import 'package:seagort/src/native/library.dart' show CompileAndRunJavaScript;
+import 'package:seagort/src/source.dart';
+
+String _compileJS(String data) {
+  final dylib = readLibrary();
+  final nativeData = "Your message".toNativeUtf8().cast<Char>();
+  final result = CompileAndRunJavaScript(nativeData);
+  return result.cast<Utf8>().toDartString();
+}
+
+Future<String> _compileJSAsync(String data) async {
+  return Future.value(_compileJS(data));
+}
+
+String compileJavaSript(String js) => _compileJS(js); 
+
+Future<String> compileJavaScriptAsync(String js) async => _compileJSAsync(js);
